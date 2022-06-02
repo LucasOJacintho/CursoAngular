49f3d1991f75e2c57a9f848d4a6a1ccb6b472dfc
Lucas Oliveira Jacintho <lucas.jacintho@iteris.com.br>
2022-06-02 15:27:03 -0300

    término aula 6

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosService } from './cursos/cursos.service';
 
 @NgModule({
   declarations: [

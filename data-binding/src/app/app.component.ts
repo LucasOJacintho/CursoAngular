331c7cc9afae2fb86d6113a6623385ffbe050f91
Lucas Oliveira Jacintho <lucas.jacintho@iteris.com.br>
2022-06-03 16:01:35 -0300

    Término Aula 16

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding';
+  valor: number = 5;
+  deletarCiclo: boolean = false;
+
+  mudarValor(){
+    this.valor++;
+  }
+
+  destruirCiclo(){
+    this.deletarCiclo = true;
+  }
+
+  criarCiclo(){
+    this.deletarCiclo = false;
+  }
 }
